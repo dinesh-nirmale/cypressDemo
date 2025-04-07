c06316852e3a6170ff0ce475fcd83bddf72a0ad1
dinesh-nirmale <dinesh-nirmale>
2025-04-07 20:01:27 +0530

    added yml

--- cypress.config.js
+++ cypress.config.js
@@ -5,5 +5,8 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/*/.cy.js',
+    supportFile: 'cypress/support/e2e.js'
   },
 });
